846254544c061c49fc37179b76c6d879779e74b4
Александр Александрович Чуносов <goldengooserider999@gmail.com>
2022-10-17 15:43:24 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    class Car
+    {
+        private int speed = 0;
+        public void PrintSpeed()
+        {
+            if (speed == 0)
+            {
+                Console.WriteLine("Stay ...");
+            }
+            if (speed > 0)
+            {
+                Console.WriteLine($"Drive forward with speed {speed} km/h");
+            }
+            if (speed < 0)
+            {
+                Console.WriteLine($"Drive backward with speed {speed} km/h");
+            }
+        }
+        public void DriveForward()
+        {
+            speed = 60;
+        }
+        public void Stop()
+        {
+            speed = 0;
+        }
+        public void DriveBackward()
+        {
+            speed = -5;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("\t== Car 1 ==");
+        var car = new Car();
+        car.PrintSpeed();
+        car.DriveForward();
+        car.PrintSpeed();
+        car.Stop();
+        car.PrintSpeed();
+        car.DriveBackward();
+        car.PrintSpeed();
+
+        Console.WriteLine("\t== Car 2 ==");
+        var car2 = new Car();
+        car2.PrintSpeed();
+        car2.DriveBackward();
+        car2.PrintSpeed();
+    }
+}
